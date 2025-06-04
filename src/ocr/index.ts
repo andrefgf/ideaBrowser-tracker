823e9b7cefbc646f59d943c9cecc0c6e23c6b3e1
André Guerra <andrefgfrancisco@gmail.com>
2025-06-04 11:55:39 +0100

    delete ocr

--- src/ocr/index.ts
+++
@@ -1,6 +0,0 @@
-import Tesseract from 'tesseract.js';
-
-export async function extractTextFromImageBuffer(buffer: Buffer): Promise<string> {
-  const { data: { text } } = await Tesseract.recognize(buffer, 'eng');
-  return text;
-}
